d7379f9a1c513da452dbeafe9a45c3469832ab89
Thuviksa <90987157+ThuviksaM@users.noreply.github.com>
2021-12-17 20:41:19 -0500

    Update math.js

--- docs/mathematics/math.js
+++ docs/mathematics/math.js
@@ -2,41 +2,41 @@
 
 /* Calculator */
 function solveForAns() {
-  if (click "btn-1" == True) {
-    var outAns = outAns+"1";
-  } else if (click "btn-2" == True) {
-    var outAns = outAns+"2";
-  } else if (click "btn-3" == True) {
-    var outAns = outAns+"3";
-  } else if (click "btn-4" == True) {
-    var outAns = outAns+"4";
-  } else if (click "btn-5" == True) {
-    var outAns = outAns+"5";
-  } else if (click "btn-6" == True) {
-    var outAns = outAns+"6";
-  } else if (click "btn-7" == True) {
-    var outAns = outAns+"7";
-  } else if (click "btn-8" == True) {
-    var outAns = outAns+"8";
-  } else if (click "btn-9" == True) {
-    var outAns = outAns+"9";
-  } else if (click "btn-0" == True) {
-    var outAns = outAns+"0";
-  } else if (click "btn-." == True) {
-    var outAns = outAns+".";
-  } else if (click "btn-+" == True) {
-    var outAns = outAns+"+";
-  } else if (click "btn-_" == True) {
-    var outAns = outAns+"-";
-  } else if (click "btn-x" == True) {
-    var outAns = outAns+"x";
-  } else if (click "btn-/" == True) {
-    var outAns = outAns+"/";
-  } else if (click "btn-=" == True) {
-    var outAns = float(outAns);
-    document.getElementById("calc-out").innerHTML = setText(outAns);
-  } else if (click "btn-c" == True) {
-    var outAns = none;
+  if (id "btn-1" == True) {
+      var outAns = outAns+"1";
+  } else if (id "btn-2" == True) {
+      var outAns = outAns+"2";
+  } else if (id "btn-3" == True) {
+      var outAns = outAns+"3";
+  } else if (id "btn-4" == True) {
+      var outAns = outAns+"4";
+  } else if (id "btn-5" == True) {
+      var outAns = outAns+"5";
+  } else if (id "btn-6" == True) {
+      var outAns = outAns+"6";
+  } else if (id "btn-7" == True) {
+      var outAns = outAns+"7";
+  } else if (id "btn-8" == True) {
+      var outAns = outAns+"8";
+  } else if (id "btn-9" == True) {
+      var outAns = outAns+"9";
+  } else if (id "btn-0" == True) {
+      var outAns = outAns+"0";
+  } else if (id "btn-." == True) {
+      var outAns = outAns+".";
+  } else if (id "btn-+" == True) {
+      var outAns = outAns+"+";
+  } else if (id "btn-_" == True) {
+      var outAns = outAns+"-";
+  } else if (id "btn-x" == True) {
+      var outAns = outAns+"x";
+  } else if (id "btn-/" == True) {
+      var outAns = outAns+"/";
+  } else if (id "btn-=" == True) {
+      var outAns = float(outAns);
+      document.getElementById("calc-out").innerHTML = setText(outAns);
+  } else if (id "btn-c" == True) {
+      var outAns = none;
   }
 };
 
