6ff315890f2fd01a0c02773fdc32b284eb3e8cc1
Thuviksa <90987157+ThuviksaM@users.noreply.github.com>
2021-12-19 12:36:19 -0500

    Update the-arts.js

--- docs/the-arts/the-arts.js
+++ docs/the-arts/the-arts.js
@@ -1,1 +1,13 @@
+/*--------------------------------------------------------------------------------------*/
 
+/* Helpful, but simple functions */
+function setText(id, text) {
+  document.getElementById(id).innerHTML = text /* Basically use actual command to set txt of something, and make it simpler */
+};
+
+function getValue(id) {
+  document.getElementById(id).value; /* Basically use actual function to get value of something, and make it simpler */
+};
+
+
+/*--------------------------------------------------------------------------------------*/
